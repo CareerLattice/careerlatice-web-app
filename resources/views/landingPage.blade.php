fabe9304bb6a2c32cd744d08f59db81b057f9842
Vincent Oliver Limarus <vincentlimarus27@gmail.com>
2024-09-30 02:30:28 +0700

    update navbar and landing page 1

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -8,6 +8,7 @@
     {{-- Bootstrap 5 --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="{{ asset('css/landingPage.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     {{-- End of Bootstrap 5 --}}
 </head>
 <body>
@@ -67,23 +68,64 @@
                         </div>
 
                         <div class="mt-3">
-                            <a href="#Companies" class="btn btn-outline-primary btn-lg mt-2">Visit All Company</a>
-                            <a href="#Jobs" class="btn btn-outline-dark btn-lg ms-2 mt-2">View Job Vacancy</a>    
+                            <a href="#Companies" class="btn btn-outline-primary btn-lg mt-3">Popular Company</a>
+                            <a href="#Jobs" class="btn btn-outline-dark btn-lg ms-2 mt-3">Popular Job Vacancy</a>    
                         </div>
                         
                     </div>
-                    <div class="col-md-4 mt-5">
-                        {{-- <img src="{{ asset('assets/LandingPage.jpg') }}" class="img-fluid" alt="CareerLatice"> --}}
+                    <div class="image col-md-4 mt-5">
+                        <img src="{{ asset('assets/.jpg') }}" class="img-fluid" alt="CareerLatice">
                     </div>
                 </div>
-            </div>
 
+                <div class="promotion row container col-sm-12">
+                    <div class="col-sm-6 col-md-3 mb-3">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <i class="bi bi-building fs-1 text-primary"></i> <!-- Bootstrap icon for company -->
+                                <h5 class="card-title mt-2">Hiring Company</h5>
+                                <p class="card-text">Over 500 companies actively seeking talented professionals.</p>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="col-sm-6 col-md-3 mb-3">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <i class="bi bi-person-lines-fill fs-1 text-info"></i> <!-- Bootstrap icon for applicant -->
+                                <h5 class="card-title mt-2">Applicants Each Year</h5>
+                                <p class="card-text">Join thousands of applicants securing career-defining opportunities annually.</p>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="col-sm-6 col-md-3 mb-3">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <i class="bi bi-people-fill fs-1 text-success"></i> <!-- Bootstrap icon for total applicants -->
+                                <h5 class="card-title mt-2">Total Applicants</h5>
+                                <p class="card-text">Over 10,000 applicants successfully placed in jobs worldwide.</p>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <div class="col-sm-6 col-md-3 mb-3">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <i class="bi bi-person-check-fill fs-1 text-warning"></i> <!-- Bootstrap icon for total users -->
+                                <h5 class="card-title mt-2">Total Users</h5>
+                                <p class="card-text">More than 50,000 users are benefiting from our career services.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
         </section>
-        
     </main>
 
     {{-- Bootstrap 5 --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB30NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     {{-- End of Bootstrap 5 --}}
