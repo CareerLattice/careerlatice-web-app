43618900aa92aec4ffe9a39f87a1e18c85860dd4
Richard Sugiharto <richardsugiharto0@gmail.com>
2024-11-19 13:55:25 +0700

    Edit reset password page

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                @auth
+                    <div class="card-header">{{ __('Change Password') }}</div>
+                @endauth
+
+                @guest
+                    <div class="card-header">{{ __('Reset Password') }}</div>
+                @endguest
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
