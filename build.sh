fe8c162be28ff13b0b50f7e812d7cdd6c89be706
Vincent Oliver Limarus <vincentlimarus27@gmail.com>
2024-12-03 23:02:28 +0700

    fix vercel.json

--- build.sh
+++
@@ -1,7 +0,0 @@
-apt-get update
-apt-get install -y libssl1.0-dev
-
-composer install --optimize-autoloader --no-dev
-
-chmod -R 775 storage bootstrap/cache
-chown -R www-data:www-data storage bootstrap/cache
