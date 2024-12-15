58ab060aab5c6508a1852fbe4afde432fa2f6942
Vincent Oliver Limarus <vincentlimarus27@gmail.com>
2024-12-15 19:31:17 +0700

    feat lang user

--- resources/lang/en/lang.php
+++ resources/lang/en/lang.php
@@ -143,5 +143,28 @@ return [
         'signUpSignUpCompany' => 'Sign Up',
     /* End of Company Page */
 
+
+    // Start of Admin Page
+        // Admin Home Page
+        'Welcome Back' => 'Welcome Back',
+        'Month Income' => 'This Month Income',
+        'Total Income' => 'Total Income',
+        'Total Applier' => 'Total Applier',
+        'Total Company' => 'Total Company',
+        'Website Income' => 'Website Income',
+        'List Premium User' => 'List of All Premium Users',
+        'From' => 'From',
+        'To' => 'To',
+        'Search' => 'Search',
+        'Export' => 'Export All Users',
+        'No' => 'No',
+        'Name' => 'Name',
+        'Revenue' => 'Revenue',
+        'Start' => 'Start Date',
+        'End' => 'End Date',
+        'No Premium' => 'No Premium User Found.'
+
+    // End of Admin Page
+
 ];
 ?>
