99266a93250be073e7d829004a83698da4b195fd
Richard Sugiharto <richardsugiharto0@gmail.com>
2024-12-28 16:19:20 +0700

    Refactor middleware registration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,15 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 
-use \App\Http\Middleware\UserAuthCheck;
-use \App\Http\Middleware\CompanyAuthCheck;
-use App\Http\Middleware\AdminAuth;
-use App\Http\Middleware\JobAuthCheck;
 use App\Http\Middleware\SetLocale;
+
+use App\Http\Middleware\CompanyAuthCheck;
+use App\Http\Middleware\AdminAuth;
+use App\Http\Middleware\UserAuthCheck;
+use App\Http\Middleware\JobAuthCheck;
+use App\Http\Middleware\JobApplicationMiddleware;
+use App\Http\Middleware\EducationMiddleware;
+use App\Http\Middleware\ExperienceMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -29,9 +33,12 @@ return Application::configure(basePath: dirname(__DIR__))
             'auth' => Authenticate::class,
             'guest' => RedirectIfAuthenticated::class,
             'company_auth' => CompanyAuthCheck::class,
+            'admin_auth' => AdminAuth::class,
             'user_auth' => UserAuthCheck::class,
             'job_auth' => JobAuthCheck::class,
-            'admin_auth' => AdminAuth::class,
+            'job_application' => JobApplicationMiddleware::class,
+            'education' => EducationMiddleware::class,
+            'experience' => ExperienceMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
