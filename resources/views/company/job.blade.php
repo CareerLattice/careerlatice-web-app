ef68da9204126a224e7050c49d555c90bf1425ea
kimson-1 <kimson.mcklain@binus.ac.id>
2025-01-17 22:42:30 +0700

    Update job.blade.php

--- resources/views/company/job.blade.php
+++ resources/views/company/job.blade.php
@@ -427,5 +427,16 @@
                 }
             });
         }
+
+        function toggleDropdown() {
+            var dropdownMenu = document.getElementById('dropdownMenu');
+            
+            if (dropdownMenu.style.display === "block") {
+                dropdownMenu.style.display = "none";
+            } else {
+                dropdownMenu.style.display = "block"; 
+            }
+        }
+
     </script>
 @endsection
